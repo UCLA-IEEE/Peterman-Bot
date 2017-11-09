8f2b0f906c4835e87f20b4ebb6f4b8158ff81c4b
Jeffrey Chan <jeffschan97@gmail.com>
2017-11-09 19:29:24 +0000

    Adjustments

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from slackclient import SlackClient
+
 def main():
-    print 'hello world'
+    with open('./config/key.txt', 'r') as key_file:
+        bot_token = key_file.read().replace('\n', '')
+
+    slack_obj = SlackClient(bot_token)
+
+    bot_id = "U0H7GEEJW"
+
+    timer = 0
+
+    if slack_obj.rtm_connect():
+        print 'hello world'
+    else:
+        print 'Connection Failed: Invalid Token'
+        exit()
 
 if __name__ == '__main__':
     main()
