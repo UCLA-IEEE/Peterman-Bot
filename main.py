9890e73162664a5dc5d22226ca66e560648ee0cb
Jeffrey Chan <jeffschan97@gmail.com>
2017-11-13 13:33:34 -0800

    Clean up

--- main.py
+++ main.py
@@ -1,43 +1,34 @@
 import time
 from slackclient import SlackClient
 
-from scanner.scan import run_scan_for_officers
-from scanner.util import get_mac_address_map
-from handler.index import handle_input
+from PetermanBot.PetermanBot import PetermanBot
 
 def main():
+    # get bot_token and bot_id
     with open('./config/key.txt', 'r') as key_file:
         bot_token = key_file.read().replace('\n', '')
 
     bot_id = "U0H7GEEJW"
 
+    # initialize slack client object
     slack_client = SlackClient(bot_token)
+    connection = slack_client.rtm_connect()
+    if not connection:
+        print 'Connection Failed: Invalid Token'
+        exit()
 
     timer = time.time()
 
-    mac_address_map = get_mac_address_map()
-    officers_in_lab = run_scan_for_officers(mac_address_map)
+    peterman_bot = PetermanBot(bot_token, bot_id, slack_client)
 
-    if slack_client.rtm_connect():
-        while True:
-            response = ''
-            event_list = slack_client.rtm_read()
-            for event in event_list:
-                if event.get('text'):
-                    user_input = event.get('text').lower().strip()
-                    response = handle_input(user_input, officers_in_lab)
+    # continuously have PetermanBot handle events
+    # every 10 seconds, have PetermanBot re-scan the network
+    while True:
+        peterman_bot.handle_events()
 
-            if time.time() - timer >= 10:
-                timer = time.time()
-                officers_in_lab = run_scan_for_officers(mac_address_map)
-
-            if response and event.get('user') != bot_id:
-                channel_id = event.get('channel')
-                slack_client.api_call('chat.postMessage', as_user='true', channel=channel_id, text=response)
-
-    else:
-        print 'Connection Failed: Invalid Token'
-        exit()
+        if time.time() - timer >= 10:
+            timer = time.time()
+            peterman_bot.run_scan_for_officers()
 
 
 if __name__ == '__main__':
